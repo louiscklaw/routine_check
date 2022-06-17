f9c4610433cf9313ba3e293ea903317d2d074efd
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-17 23:56:04 +0800

    update louislabs-lighthouse-check,

--- louislabs-lighthouse-check/test.sh
+++ louislabs-lighthouse-check/test.sh
@@ -3,6 +3,6 @@
 set -ex
 
 docker run --rm --name lighthouse -it \
-  -v $PWD/volumes/report:/home/chrome/reports \
+  -v $PWD/volumes/reports:/home/chrome/reports \
   --cap-add=SYS_ADMIN femtopixel/google-lighthouse \
-  http://www.example.com
+  https://portfolio.iamon99.com/
