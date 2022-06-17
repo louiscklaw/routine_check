215c9ca8506ec94005a759ebd4cee87317e41fc4
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-17 20:06:01 +0800

    update routine-check,

---
+++ louislabs-lighthouse-check/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+docker run --rm --name lighthouse -it \
+  -v /path/to/your/report:/home/chrome/reports \
+  --cap-add=SYS_ADMIN femtopixel/google-lighthouse \
+  <your_site_url <optional_args>
+
