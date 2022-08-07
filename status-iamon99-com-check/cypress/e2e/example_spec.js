20b0ec848d98c5bd3e1fde69c2c211e149b576c4
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-07 13:59:27 +0800

    update and test status.iamon99.com,

--- status-iamon99-com-check/cypress/e2e/example_spec.js
+++ status-iamon99-com-check/cypress/e2e/example_spec.js
@@ -2,12 +2,8 @@ const view_ports = ['macbook-16'];
 
 const orientations = ['portrait', 'landscape'];
 
-describe('viewports', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
-  });
-
-  it('test viewport and orientation', () => {
+describe('status.iamon99.com', () => {
+  it('test https://status.iamon99.com', () => {
     for (let i = 0; i < view_ports.length; i++) {
       let view_port = view_ports[i];
       for (let ii = 0; ii < orientations.length; ii++) {
@@ -16,7 +12,9 @@ describe('viewports', () => {
         cy.viewport(view_port, orientation);
         cy.visit('https://status.iamon99.com');
 
-        cy.get('#my-name').should('have.text', 'Louis Law');
+        cy.get('.title', { timeout: 10000 }).should('be.visible');
+
+        cy.get('.title').should('have.text', 'Uptime Kuma');
 
         cy.screenshot();
 
