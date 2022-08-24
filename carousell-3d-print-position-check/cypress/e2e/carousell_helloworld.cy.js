44b5aeb8504c5a2d7c2cd08a0577824547e74d81
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-24 18:05:39 +0800

    update viewport,

--- carousell-3d-print-position-check/cypress/e2e/carousell_helloworld.cy.js
+++ carousell-3d-print-position-check/cypress/e2e/carousell_helloworld.cy.js
@@ -1,6 +1,7 @@
 describe('My First Test https://www.carousell.com.hk/', () => {
   it('Visits https://www.carousell.com.hk/', () => {
     cy.visit('https://www.carousell.com.hk/');
+    cy.viewport('macbook-16');
 
     cy.screenshot();
   });
