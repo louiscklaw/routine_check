5d2f8c6d47a29ab4303325cbd7658be9b353bf60
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-24 17:34:25 +0800

    update upload videos,

--- carousell-3d-print-position-check/cypress/e2e/3D_代客打印.cy.js
+++ carousell-3d-print-position-check/cypress/e2e/3D_代客打印.cy.js
@@ -7,7 +7,6 @@ describe('3D 代客打印 - my post should appears in the first 8 cards', () => 
     cy.viewport('macbook-16');
 
     cy.visit('https://www.carousell.com.hk');
-    cy.wait(3000);
 
     cy.xpath('(.//input[@placeholder="Search for an item"])[1]').type('3D 代客打印');
 
@@ -26,5 +25,9 @@ describe('3D 代客打印 - my post should appears in the first 8 cards', () => 
       });
 
     // cy.screenshot();
+  });
+
+  after(() => {
+    cy.wait(3000);
   });
 });
