9441ef239b664728222b87d65a0e8fb3c70b7bcb
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-24 18:12:09 +0800

    update problem,

--- carousell-3d-print-position-check/cypress/e2e/carousell_helloworld.cy.js
+++ carousell-3d-print-position-check/cypress/e2e/carousell_helloworld.cy.js
@@ -2,6 +2,8 @@ describe('My First Test https://www.carousell.com.hk/', () => {
   it('Visits https://www.carousell.com.hk/', () => {
     cy.visit('https://www.carousell.com.hk/');
 
+    cy.xpath('(.//input[@placeholder="Search for an item"])[1]').type('3D 代客打印');
+
     cy.screenshot();
   });
 });
