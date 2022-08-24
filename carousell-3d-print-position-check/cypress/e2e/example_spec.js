9b8f78aa863e7b62fbcba8ae0c58c233a6a08331
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-24 16:52:54 +0800

    update,

--- carousell-3d-print-position-check/cypress/e2e/example_spec.js
+++ carousell-3d-print-position-check/cypress/e2e/example_spec.js
@@ -1,22 +1,27 @@
-// https://docs.cypress.io/api/commands/find
-import sha256 from 'sha256';
+// 3D%20代客打印
 
-describe('www_example_com', () => {
-  it('landing login page', () => {
-    cy.viewport(1920, 5080);
-    cy.visit('http://www.example.com/');
-  });
+describe('3D 代客打印 - my post should appears in the first 8 cards', () => {
+  it('Visits https://www.carousell.com.hk', () => {
+    cy.viewport('macbook-16');
 
-  it('example check changed', () => {
-    cy.get('body').then(function ($elem) {
-      // text ignore and cleaning here
-      // text ignore and cleaning here
+    cy.visit('https://www.carousell.com.hk');
 
-      if (sha256($elem.text()) != '690fe5ea036d21a8ccda0c32c04c608f80583b65aac740a70011e2289316fd3a') {
-        cy.log('text changed');
-        cy.log(sha256($elem.text()));
-        cy.log($elem.text());
-      }
-    });
+    cy.xpath('(.//input[@placeholder="Search for an item"])[1]').type('3D 代客打印');
+
+    cy.xpath('(.//button[@data-testid="navbar-search-input-location-desktop-btn-search"])[1]').click();
+    cy.wait(5000);
+
+    Array(8)
+      .fill(0)
+      .map((_, idx) => {
+        let el_idx = idx + 1;
+        cy.xpath(
+          `(.//div[(@data-testid!="listing-card") and starts-with(@data-testid,"listing-card")]/div/a)[${el_idx}]`
+        ).then(($ele) => {
+          cy.debug($ele.text());
+        });
+      });
+
+    // cy.screenshot();
   });
 });
