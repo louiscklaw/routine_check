94e692ec86862aa03afbf9b5acd441a4b28e966d
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-24 16:53:50 +0800

    update,

--- carousell-3d-print-position-check/cypress/e2e/example_spec.js
+++ carousell-3d-print-position-check/cypress/e2e/example_spec.js
@@ -1,10 +1,12 @@
 // 3D%20代客打印
+let TEST_STUB = 'https://www.carousell.com.hk';
+// let TEST_STUB = "http://localhost:3000";
 
 describe('3D 代客打印 - my post should appears in the first 8 cards', () => {
-  it('Visits https://www.carousell.com.hk', () => {
+  it(`Visits ${TEST_STUB}`, () => {
     cy.viewport('macbook-16');
 
-    cy.visit('https://www.carousell.com.hk');
+    cy.visit(TEST_STUB);
 
     cy.xpath('(.//input[@placeholder="Search for an item"])[1]').type('3D 代客打印');
 
